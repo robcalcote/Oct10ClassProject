de0891b8f96f14bdd35fc50fb3cf6e5103dbaf2a
Rob Calcote <robcalcote@gmail.com>
2018-10-10 15:10:55 -0600

    Added comments for clarification

--- Oct10ClassProject/Controllers/HomeController.cs
+++ Oct10ClassProject/Controllers/HomeController.cs
@@ -11,7 +11,11 @@ namespace Oct10ClassProject.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            // This calls the view with the 'Test' name in the ../Views/Home/Test directory
             return View("Test");
+
+            // This would call the view with 'default' name ../Views/Home/Index (same name as method) directory
+            //return View();
         }
     }
 }
